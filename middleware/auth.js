126d12a8e36a1a594270eb0d629aa8778610ec0f
John Ekunola <tobiz2002@gmail.com>
2019-10-20 01:55:57 +0100

    feat(auth): setup auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const validateAuth = resolver => (...args) => {
+  if (!args[2].auth.isAuthenticated) {
+    throw new Error(`Invalid authentication token`);
+  }
+
+  return resolver.apply(null, args);
+};
+
+const getUserFromAuthToken = (req, res, next) => {
+  req.auth = {
+    isAuthenticated: false,
+    currentUser: null
+  };
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const { user } = jwt.verify(token, "mysecret");
+    req.auth.isAuthenticated = true;
+    req.auth.currentUser = user;
+  } catch (error) {
+    console.warn(`error verifying token: ${error}`);
+  }
+  next();
+};
+
+module.exports = {
+  validateAuth,
+  getUserFromAuthToken
+};
