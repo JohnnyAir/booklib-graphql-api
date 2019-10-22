07cf56c777680d4125c38d7d620c40010f952216
John Ekunola <tobiz2002@gmail.com>
2019-10-22 19:27:08 +0100

    feat(glob): require all mutations in the folder

--- mutation/index.js
+++ mutation/index.js
@@ -1,11 +1,22 @@
 const { GraphQLObjectType } = require("graphql");
-const userMutations = require("./User");
+const glob = require("glob");
+const cwd = "./mutation";
+const pattern = "**/*.js";
 
-const Mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    ...userMutations
-  }
+const query_modules = glob.sync(pattern, { cwd });
+
+const queries = {};
+
+query_modules.forEach(filepath => {
+  const module_queries = /index.js/.test(filepath)
+    ? {}
+    : require(`./${filepath}`);
+  Object.assign(queries, module_queries);
 });
 
-module.exports = Mutation;
+module.exports = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    ...queries
+  }
+});
